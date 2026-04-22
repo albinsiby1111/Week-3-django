2a3e9d06574607d99d985fe0fa947df6fd8bca96
Albin Siby <sibyt4605@gmail.com>
2026-04-22 11:08:02 +0530

    Day 13 Views & Templates completed

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('course/<str:name>/', views.course_detail, name='course_detail'),
+    path('', views.course_list),  
+    path('course/<str:name>/', views.detail_view),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
+def home_view(request):
     return HttpResponse("Welcome to Klaw Courses")
 
-def course_detail(request, name):
+def detail_view(request, name):
     return HttpResponse(f"This is course: {name}")
+
+def course_list(request):
+    context = {
+        'title': 'Available Courses',
+        'user_name': 'Albin Siby',
+        'courses': ['Python', 'Django', 'HTML', 'CSS']
+    }
+    return render(request, 'courses/course_list.html', context)
