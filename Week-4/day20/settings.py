2ed0c10c6f50f2c61bf3b16491d70bded3109608
Albin Siby <sibyt4605@gmail.com>
2026-05-01 13:05:43 +0530

    Day 20 Authentication & Permissions

---
+++ Week-4/day20/settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
